3982cc06fe93771ea1d3253254d6bab46a38148e
Brandon Weaver <a7xncob@gmail.com>
2019-12-28 18:46:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get "/reversename/:name" do
+    params[:name].reverse
+  end
+  
   get "/square/:number" do
     (params[:number].to_i * params[:number].to_i).to_s
   end
